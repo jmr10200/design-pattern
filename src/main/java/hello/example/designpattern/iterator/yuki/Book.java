34105b22d0ff60d66717955574c8ba58a395fe3b
jmr10200 <jmr10200@gmail.com>
2023-11-01 17:53:39 +0900

    java 언어로 배우는 디자인패턴 입문 - 이터레이터 패턴

---
+++ src/main/java/hello/example/designpattern/iterator/yuki/Book.java
@@ -0,0 +1,14 @@
+package hello.example.designpattern.iterator.yuki;
+
+public class Book {
+
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/hello/example/designpattern/iterator/yuki/BookTest.java
@@ -0,0 +1,33 @@
+package hello.example.designpattern.iterator.yuki;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    void main() {
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("1.bible"));
+        bookShelf.appendBook(new Book("2.design patterns"));
+        bookShelf.appendBook(new Book("3.javascript basic"));
+        bookShelf.appendBook(new Book("4.spring framework basic"));
+
+        // 명시적으로 Iterator 를 사용하는 방법
+        Iterator<Book> iter = bookShelf.iterator();
+        while (iter.hasNext()) {
+            Book book = iter.next();
+            System.out.println(book.getName());
+        }
+        System.out.println("---");
+
+        // 확장 for 문 사용하는 방법
+        for (Book book : bookShelf) {
+            System.out.println(book.getName());
+        }
+    }
+
+}
