5e2bfc2c1eb77b33124acc89f32999ed9e678855
jmr <jmr10200@gmail.com>
2023-11-30 16:12:45 +0900

    프로토타입 패턴 연습문제 6-2 복사 생성자 이용 코멘트 수정

--- src/main/java/hello/example/designpattern/prototype/yuki/UnderlinePen.java
+++ src/main/java/hello/example/designpattern/prototype/yuki/UnderlinePen.java
@@ -10,7 +10,7 @@ public class UnderlinePen implements Product {
         this.ulchar = ulchar;
     }
 
-    // 복사 생성자
+    // 복사 생성자: 같은 클래스의 인스턴스를 인수로 가지고 인스턴스 생성 시 필드를 복사하는 생성자
     public UnderlinePen(UnderlinePen underlinePen) {
         this.ulchar = underlinePen.ulchar;
     }
@@ -37,7 +37,6 @@ public class UnderlinePen implements Product {
 //    }
 
     // 복사 생성자 이용
-
     @Override
     public Product createCopy() {
         return new UnderlinePen(this);
