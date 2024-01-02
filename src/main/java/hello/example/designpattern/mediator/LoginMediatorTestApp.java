87bb84af12ffdb9d481c91e7c334f67ddc5dd42c
jmr <jmr10200@gmail.com>
2024-01-02 23:22:54 +0900

    테스트용 메인 클래스 이름 수정

--- src/main/java/hello/example/designpattern/mediator/LoginMediatorTestApp.java
+++ src/main/java/hello/example/designpattern/mediator/LoginMediatorTestApp.java
@@ -2,7 +2,7 @@ package hello.example.designpattern.mediator;
 
 import hello.example.designpattern.mediator.yuki.LoginFrame;
 
-public class LoginMediatorMainTest {
+public class LoginMediatorTestApp {
     public static void main(String[] args) {
         // 실행
         new LoginFrame("Mediator Main test");
